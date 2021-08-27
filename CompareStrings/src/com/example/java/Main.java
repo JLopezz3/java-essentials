5873402d375273998fece04a6492ce83f451eaa3
JLopezz3 <lopezjerel@gmail.com>
2021-08-27 12:42:05 -0500

    second commit

---
+++ CompareStrings/src/com/example/java/Main.java
@@ -0,0 +1,26 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String s1 = "Hello!";
+        String s2 = "Hello!";
+
+        if (s1 == s2) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+
+        String s3 = new String("Hello!");
+        String s4 = new String("HELLO!");
+        if (s3.equalsIgnoreCase(s4)) {
+            System.out.println("They match!");
+        } else {
+            System.out.println("They don't match!");
+        }
+
+    }
+}
---
+++ IfElse/src/com/example/java/Main.java
@@ -0,0 +1,18 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int monthNumber = 7;
+
+        if (monthNumber >= 0 && monthNumber <= 2) {
+            System.out.println("You're in Quarter 1!");
+        } else if (monthNumber >= 3 && monthNumber <= 5) {
+            System.out.println("You're in Quarter 2!");
+        } else {
+            System.out.println("It's the second half of the year!");
+        }
+
+    }
+}
