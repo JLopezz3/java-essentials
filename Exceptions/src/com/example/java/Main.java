3c411e86289f4395151e1842fdf61391cfe1d088
JLopezz3 <lopezjerel@gmail.com>
2021-09-01 13:21:52 -0500

    Exceptions

---
+++ Exceptions/src/com/example/java/Main.java
@@ -0,0 +1,23 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        SYNTAX VS Exception
+//        Syntax = Prevents code from running
+//        Exception = runtime errors
+
+
+//        String welcome = "Welcome!";
+//        char[] chars = welcome.toCharArray();
+//        char lastChar = chars[chars.length];
+//        System.out.println("last char: " + lastChar);
+
+//        String nothing = null;
+//        System.out.println(nothing);
+//        System.out.println("string length=" + nothing.length());
+
+
+    }
+}
